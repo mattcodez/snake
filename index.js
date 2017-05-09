f55d2be041f951f5377bc627d49fa5ca9a1f5e90
Matt Molnar <matthew.molnar@gmail.com>
2017-05-09 15:12:28 -0400

    Work on main render

--- index.js
+++ index.js
@@ -10,29 +10,76 @@ class game {
   //TODO: remember constants need to be in separate file, does that make sense here?
 
   constructor(window, document, playSpaceEl){
-    this.stateUpdateSpeed   = 10;  //ms between game state changes
-    this.snakeSpeed    = 4;   //How many state updates between snake movement
-    this.state         = {};  //Holds game state for updates and rendering
+    this.stateUpdateSpeed = 10;  //ms between game state changes
+    this.snakeSpeed       = 4;   //How many state updates between snake movement /*TODO: move to snake stats*/
+    this.state            = {};  //Holds game state for updates and rendering
+    this.gridSize         = 100;
 
-    this.mainWindow    = window;
-    this.mainHTMLDoc   = document;
-    this.playSpaceEl   = playSpaceEl;
+    //snake stats
+    //TODO: consider separate class
+    this.snake            = {};
+    const snakeStart      = Math.floor(this.gridSize / 2);
+    this.snake.pos        = [snakeStart, snakeStart];
+    this.snake.len        = 2;
+    this.snake.dir        = 0; //0: up, 1: right, 2: down, 3: left
+
+    this.mainWindow       = window;
+    this.mainHTMLDoc      = document;
+    this.playSpaceEl      = playSpaceEl;
 
     this.assignEventHandlers();
-    this.buildInitialPlayData();
+    //this.buildInitialPlayData();
   }
 
   assignEventHandlers(){
     //game state and render
-    this.window.requestanimationframe(this.render);
-    this.window.setInterval(this.updateState, this.stateUpdateSpeed);
+    this.mainWindow.setInterval(this.updateState.bind(this), this.stateUpdateSpeed);
+    this.mainWindow.requestAnimationFrame(function runRender(timestamp){
+      this.render();
+      this.mainWindow.requestAnimationFrame(runRender.bind(this));
+    }.bind(this));
 
     //controls
+    //TODO
   }
 
-  buildInitialPlayData(){
-    this.playSpaceEl
+  // buildInitialPlayData(){
+  //   const grid = []
+  //   this.state.grid = grid;
+  // }
+
+  updateState(){
+    //TODO: add food
+    const [x, y] = this.snake.pos;
+    //this.snake.pos = [x, y + 1];
   }
 
-  render(timestamp)
+  render(){
+    const {len, pos} = this.snake;
+    const [x, y] = pos;
+    function append(text){
+      this.playSpaceEl.innerHTML += text;
+    }
+
+    this.playSpaceEl.innerHTML = '';
+    for (let row = 0; row < this.gridSize; row++){
+      for (let col = 0; col < this.gridSize; col++){
+        switch(this.snake.dir){
+          case 0: //up
+            if (col === x && (row <= y && row >= (y - len))){
+              append('S');
+            }
+            else append('0');
+          break;
+          case 1: //right
+          break;
+          case 2: //down
+          break;
+          case 3: //left
+          break;
+        }
+      }
+      this.playSpaceEl.innerHTML += '<br/>'; //end of grid row
+    }
+  }
 }
